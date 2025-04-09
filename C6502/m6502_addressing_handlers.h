afa2541c4acc7538f4265696a8c9854ee4d415b4
ElectronixTM <etdiary1@gmail.com>
2025-04-09 13:24:06 +0300

    feat: декларировал функцию получения обработчиков по элементу перечисления

--- C6502/m6502_addressing_handlers.h
+++ C6502/m6502_addressing_handlers.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 #include <C6502.h>
+#include "m6502_opcodes.h"
 
 
 /**
@@ -39,5 +40,11 @@ struct m6502_Operands{
  * обращаться к шине
  */
 typedef struct m6502_Operands(*ADDRESSING_HANDLER)(M6502_HANDLE);
+
+/**
+ * Функция, принимающая опкод и возвращающая обработчик для соответствующего
+ * режима адрессации
+ */
+ADDRESSING_HANDLER get_addressing_handler(enum m6502_AddressingModes);
 
 #endif
