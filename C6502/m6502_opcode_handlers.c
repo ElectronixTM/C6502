4eefeabf020d8abeae7995d3970b03b7e4df410d
ElectronixTM <etdiary1@gmail.com>
2025-04-11 21:04:06 +0300

    sync: переношу изменения между устройствами
    Если вы это читаете, занчит я зыбыл сделать rebase

--- C6502/m6502_opcode_handlers.c
+++ C6502/m6502_opcode_handlers.c
@@ -1,6 +1,7 @@
 #include "m6502_opcode_handlers.h"
 #include "m6502_chip_instance.h"
 #include "m6502_addressing_handlers.h"
+#include "m6502_memory_utils.h"
 
 #define SKIP_UNTIL_ZERO(n)\
   if ((n) > 0)            \
@@ -20,10 +21,7 @@ RawOperands get_raw_operands(M6502_HANDLE handle, uint8_t instrsize)
   RawOperands operands;
   for (uint8_t i = 0; i < instrsize-1; i++)
   {
-    operands.bytes[i] = handle->bus.read(
-        handle->bus.handle,
-        handle->state.pc + i + 1
-        );
+    operands.bytes[i] = read_byte(handle, handle->state.pc + i + 1); 
   }
   return operands;
 }
@@ -44,7 +42,25 @@ int handle_ADC(M6502_HANDLE handle, const struct m6502_OpCodeDesc* desc)
   struct m6502_Operands operands = addressing_handler(
       handle,
       get_raw_operands(handle, desc->instrsize).word,
-      &extra_cycles);
+      &extra_cycles
+      );
+  uint8_t data;
+  uint16_t real_addr;
+  switch(operands.type)
+  {
+    case RESULT_DATA:
+      data = operands.content.data;
+      break;
+    case RESULT_ADDR:
+      data = handle->bus.read(handle->bus.handle, operands.content.address);
+      break;
+    case RESULT_IND:
+      real_addr = read_word(handle, operands.content.address);
+      data = read_byte(handle, real_addr);
+      break;
+    case RESULT_IMPL:
+      return M6502_ERR;
+  }
   return M6502_OK;
 }
 
