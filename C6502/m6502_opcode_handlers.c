54a37c4f9278c981be2def3a4f2d96c2fd61791f
Miheev Egor <etdiary1@gmail.com>
2025-04-12 23:32:49 +0300

    feat: выделил преамбулу арифметической операции
    Все арифметические операции в том или ином виде производят выборку операции
    из памяти, обрабатывают количество тактов до начала операции и выдают
    статус-коды ошибок

--- C6502/m6502_opcode_handlers.c
+++ C6502/m6502_opcode_handlers.c
@@ -58,15 +58,32 @@ int _ariphmetic_get_data(const M6502_HANDLE handle,
 
 int handle_NOP(M6502_HANDLE handle, const struct m6502_OpCodeDesc* desc)
 {
-  SKIP_UNTIL_ZERO(handle->state.pc);
+  SKIP_UNTIL_ZERO(handle->cycles_remaining);
   handle->state.pc += desc->instrsize;
   handle->cycles_remaining = desc->minrequiredcycles;
   return 0;
 }
 
-int handle_ADC(M6502_HANDLE handle, const struct m6502_OpCodeDesc* desc)
+/**
+ * Структура, аккумулирующая информацию о результате выборки операндов
+ * из памяти. Содержит информацию о выбранном операнде, о потраченных
+ * на выборку дополнительных циклов (появляются в некоторых видах адресации)
+ * и о коде возврата из функции. Предполагается, что передается через стек
+ */
+struct ParsedData
 {
-  SKIP_UNTIL_ZERO(handle->state.pc);
+  uint8_t data;
+  uint8_t extra_cycles;
+  int return_code;
+};
+
+/**
+ * Производит выборку операндов из памяти и возвращает их в структуре вместе
+ * с кодом возврата и дополнительными циклами, потраченными функцией адресации
+ */
+struct ParsedData _parse_data(const M6502_HANDLE handle,
+                               const struct m6502_OpCodeDesc* desc)
+{
   ADDRESSING_HANDLER addressing_handler = m6502_get_addressing_handler(desc->addressmode);
   uint8_t extra_cycles;
   struct m6502_Operands operands = addressing_handler(
@@ -76,18 +93,50 @@ int handle_ADC(M6502_HANDLE handle, const struct m6502_OpCodeDesc* desc)
       );
   uint8_t data;
   int ret_code = _ariphmetic_get_data(handle, &operands, &data);
-  handle->state.a += data + M6502_GET_C(handle->state.sr);
+  struct ParsedData result = {data, extra_cycles, 0};
+  if (ret_code != M6502_OK)
+  {
+    result.return_code = M6502_ERR;
+  }
+  return result;
+}
+
+// Очень некрасивая прембула, которая выполняет набор необходимых действий
+// для выполнения почти любой арифметической операции в эмуляторе:
+// Подсчитывает такты до начала выполнения операции, парсит операнды функции
+// и выдает ошибку, если операнд implied, поскольку для арифметической операции
+// это в основном не имеет смысла, а остальные случаи можно обработать руками
+#define ARIPHMETIC_PREAMBLE(handle, desc, parsed_data_reciever)            \
+  SKIP_UNTIL_ZERO((handle)->cycles_remaining);                             \
+  struct ParsedData (parsed_data_reciever) = _parse_data((handle), (desc));\
+  if ((parsed_data_reciever).return_code != 0)                             \
+  {                                                                        \
+    return M6502_ERR;                                                      \
+  }
+
+
+int handle_ADC(M6502_HANDLE handle, const struct m6502_OpCodeDesc* desc)
+{
+  ARIPHMETIC_PREAMBLE(handle, desc, parsed);
+  handle->state.a += parsed.data + M6502_GET_C(handle->state.sr);
   return M6502_OK;
 }
+
+// Макрос, генерирующий очередной выход функции по названию мнемоники.
+// Работает в предположении, что каждая мнемоника обрабатывается фунцией
+// handle_(mnemonic_name), e.g. handle_ADC, handle_LDA, ...
+#define ADD_HANDLER(mnemonic_name)\
+  case MNEM_##mnemonic_name:      \
+    return handle_##mnemonic_name;\
+    break;
 
 OPCODE_HANDLER m6502_get_opcode_handler(enum m6502_Mnemonic mnemonic)
 {
   // Сейчас я использую максимально простую реализацию
   switch(mnemonic)
   {
-    case MNEM_ADC:
-      return handle_ADC;
-      break;
+    ADD_HANDLER(ADC);
+    ADD_HANDLER(NOP);
     default:
       return handle_NOP;
   }
